b21c72224a197483ddf9526890911c9b70964fc9
Leonardo Rodrigues <lrodlima@gmail.com>
2019-10-13 09:05:38 -0300

    Adding the first commit to repo with some important configurations

---
+++ src/main/java/br/com/codenation/errordashboard/ErrordashboardApplication.java
@@ -0,0 +1,13 @@
+package br.com.codenation.errordashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrordashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrordashboardApplication.class, args);
+	}
+
+}

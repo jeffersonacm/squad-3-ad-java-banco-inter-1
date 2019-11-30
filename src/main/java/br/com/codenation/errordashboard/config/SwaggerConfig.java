b7b0b10f53a2ef8fd1be7042dc8711d77b78223c
Leonardo Rodrigues Lima <lrodlima@gmail.com>
2019-11-30 17:54:51 -0300

    Adding the swagger Docs to the project

--- src/main/java/br/com/codenation/errordashboard/config/SwaggerConfig.java
+++ src/main/java/br/com/codenation/errordashboard/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package br.com.codenation.errordashboard.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,25 +16,34 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket errorApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.codenation.errordashboard.endpoints   "))
+                .apis(RequestHandlerSelectors.basePackage("br.com.codenation.errordashboard"))
                 .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiInfo());
+                .build()
+                .apiInfo(metaData());
     }
 
-    private ApiInfo apiInfo() {
+    private ApiInfo metaData() {
         return new ApiInfo("Error Dashboard API",
-                "Codenation Final Project - An Error Dashboard Application made with Spring Boot",
+                "Codenation Final Project - An Error Dashboard API REST Application made with Spring Boot",
                 "1.0.0-RELEASE",
                 "Terms of service",
                 new Contact("Leonardo Rodrigues", "", "lrodlim@gmail.com"),
-                "License API",
-                "License URL",
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0\"",
                 Collections.emptyList());
     }
 
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
--- src/main/java/br/com/codenation/errordashboard/endpoints/UserController.java
+++ src/main/java/br/com/codenation/errordashboard/endpoints/UserController.java
@@ -3,27 +3,26 @@ package br.com.codenation.errordashboard.endpoints;
 import br.com.codenation.errordashboard.domain.entity.User;
 import br.com.codenation.errordashboard.exceptions.UserNotFoundException;
 import br.com.codenation.errordashboard.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/springjwt")
+@RequestMapping("/users")
+@Api(value = "users")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
-    @Autowired
-    public UserController(UserService userService){
-        this.userService = userService;
-    }
-
-    @GetMapping("/users/{username}")
-    @PreAuthorize("hasAuthority")
-    private User getUser(@PathVariable("username") String userName) {
-        return userService.getUserDetails(userName);
+    @ApiOperation(value = "Get User by email")
+    @GetMapping(value = "/{email}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    private User getUser(@PathVariable("email") String email) {
+        return userService.getUserDetails(email);
     }
 
     @ExceptionHandler

ff4cc4bdbb663626901e9c765f14f7d22cbefcaf
Leonardo Rodrigues Lima <lrodlima@gmail.com>
2019-12-14 11:15:21 -0300

    Security Initial configuration

---
+++ src/main/java/br/com/codenation/errordashboard/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package br.com.codenation.errordashboard.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SecurityConfig {
+}

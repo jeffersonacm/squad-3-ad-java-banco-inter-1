a1649b6824b9ce55206861df1bba0d30a57ef12b
jeffersonacm <jeffersonacm@hotmail.com>
2019-12-17 19:43:37 -0300

    Add and fix interfaces

--- src/main/java/br/com/codenation/errordashboard/service/interfaces/UserServiceInterface.java
+++ src/main/java/br/com/codenation/errordashboard/service/interfaces/UserServiceInterface.java
@@ -3,11 +3,9 @@ package br.com.codenation.errordashboard.service.interfaces;
 import br.com.codenation.errordashboard.domain.dto.UserDTO;
 import br.com.codenation.errordashboard.domain.entity.User;
 
-import java.util.List;
-
 public interface UserServiceInterface {
 
     User getUserDetails(String email);
 
-    //UserDTO createUser(String name, String email, String password);
+    UserDTO save(User user);
 }

68d266a5a5481e8b164cbebf421eb8bba6274c03
Leonardo Rodrigues Lima <lrodlima@gmail.com>
2019-12-21 06:55:10 -0300

    Adding the testing configurations

--- src/test/java/br/com/codenation/errordashboard/UserDAOTest.java
+++ src/test/java/br/com/codenation/errordashboard/UserDAOTest.java
@@ -5,15 +5,18 @@ import br.com.codenation.errordashboard.domain.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 public class UserDAOTest {
 
@@ -24,17 +27,18 @@ public class UserDAOTest {
     private TestEntityManager entityManager;
 
     @Test
-    public void findByName_returnsUserDetails() throws Exception{
-      /*  User savedUser = entityManager.persistFlushFind(new User(1L, "Leonardo",
-                "lrodlima@gmail.com", "123456", LocalDateTime.now()));
-        User user = userDAO.findByName("Leonardo");
+    public void whenFindByEmail_thenReturnUser() {
+        //given
+        User alex = new User(1L, "Alex", "alex@gmail.com",
+                "12345667", "", LocalDateTime.now(), new ArrayList<>());
+        userDAO.save(alex);
 
-        assertThat(user.getName()).isEqualTo(savedUser.getName());
-        assertThat(user.getPassword()).isEqualTo(savedUser.getPassword());
-    */}
 
-    @Test
-    public void findByEmail_returnUserDetails() {
-        //User
+        //when
+        User userFound = userDAO.findByEmail(alex.getEmail());
+
+        //then
+        assertThat(userFound.getEmail())
+                .isEqualTo(alex.getEmail());
     }
 }
